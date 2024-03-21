834d4091f423a24188b8d6bdc5404fa87bcff6bc
Whitney Woelmer <wwoelmer@vt.edu>
2024-03-22 11:38:09 +1300

    update alum data processing, add okaro

--- scripts/moving_window_cross_lake/04_process_alum_data_all_lakes.R
+++ scripts/moving_window_cross_lake/04_process_alum_data_all_lakes.R
@@ -2,6 +2,7 @@
 
 library(readxl)
 
+############################################################################################
 # rotoehu
 al <- read_excel('./data/raw_data/Daily aluminium to Rotoehu.xlsx')
 al <- al[,1:4]
@@ -11,19 +12,17 @@ colnames(al) <- c('date', 'kg_aluminum_day', 'kg_alum_day', 'L_alum_day')
 ggplot(al, aes(x = as.Date(date), y = L_alum_day)) +
   geom_line()
 
-al <- read.csv('./data/processed_data/alum_dosing_rotoehu_2011_2022.csv')
 al$date <- as.Date(al$date)
 al <- al %>% 
   select(date, L_alum_day) 
 
-ggplot(al, aes(x = date, y  = L_alum_day)) +
-  geom_point(size = 2)
-
 # sum the amount of alum dosed since last sampling dates
+df <- read.csv('./data/moving_window_analysis_cross_lake/BoP_WQ_formatted.csv') %>% 
+  filter(lake=='Rotoehu')
 dates <- unique(df$date)
 al_sum <- data.frame()
 
-for(i in 134:length(dates)){ # start at 134 before no alum was dosed before dates[134]
+for(i in 2:length(dates)){ 
   sub <- al %>% 
     filter(date <= dates[i] & date > dates[i-1]) %>% 
     mutate(sum_alum = sum(L_alum_day), 
@@ -38,10 +37,16 @@ al_sum <- al_sum %>%
   select(-date) %>% 
   rename(date = sample_date)
 
-ggplot(al_sum, aes(x = date, y  = sum_alum)) +
-  geom_point(size = 2)
+ggplot(al_sum, aes(x = as.Date(date), y  = sum_alum)) +
+  geom_point(size = 2) +
+  geom_line()
 
-
+############################################################################################
 # okaro
+# data come from email from Justine Randall at BoPRC
 
+okaro_al <- read.csv('./data/raw_data/okaro_alum_dosing.csv')
+
+
+############################################################################################
 # combine into one data file
