f5915f68df83e49a449023714f4c0c9aa6f9dba2
Whitney Woelmer <wwoelmer@vt.edu>
2024-04-09 11:40:04 +1200

    add okaro alum data

--- scripts/moving_window_cross_lake/04_process_alum_data_all_lakes.R
+++ scripts/moving_window_cross_lake/04_process_alum_data_all_lakes.R
@@ -14,7 +14,10 @@ ggplot(al, aes(x = as.Date(date), y = L_alum_day)) +
 
 al$date <- as.Date(al$date)
 al <- al %>% 
-  select(date, L_alum_day) 
+  select(date, L_alum_day) %>% 
+  rename(alum = L_alum_day) %>% 
+  mutate(note = 'alum_units_L_day',
+         lake = 'Rotoehu')
 
 # sum the amount of alum dosed since last sampling dates
 df <- read.csv('./data/moving_window_analysis_cross_lake/BoP_WQ_formatted.csv') %>% 
@@ -25,9 +28,9 @@ al_sum <- data.frame()
 for(i in 2:length(dates)){ 
   sub <- al %>% 
     filter(date <= dates[i] & date > dates[i-1]) %>% 
-    mutate(sum_alum = sum(L_alum_day), 
+    mutate(sum_alum = sum(alum), 
            sample_date = dates[i]) %>% 
-    select(-L_alum_day)
+    select(-alum)
   al_sum <- rbind(al_sum, sub)
   
 }
@@ -38,15 +41,58 @@ al_sum <- al_sum %>%
   rename(date = sample_date)
 
 ggplot(al_sum, aes(x = as.Date(date), y  = sum_alum)) +
+  facet_wrap(~lake, scales = 'free') +
   geom_point(size = 2) +
   geom_line()
 
 ############################################################################################
 # okaro
-# data come from email from Justine Randall at BoPRC
+# data came from email from Justine Randall at BoPRC
 
 okaro_al <- read.csv('./data/raw_data/okaro_alum_dosing.csv')
+okaro_al <- okaro_al %>% 
+  select(date, placeholder_tonnes_alum) %>% 
+  rename(alum = placeholder_tonnes_alum) %>% 
+  mutate(note = 'alum_units_tonnes',
+         lake = 'Okaro',
+         date = as.Date(date))
 
+okaro_al <- na.omit(okaro_al)
+
+# sum dosing which occured in each month since last sampling date
+df <- read.csv('./data/moving_window_analysis_cross_lake/BoP_WQ_formatted.csv') %>% 
+  filter(lake=='Okaro')
+dates_o <- unique(df$date)
+al_sum_okaro <- data.frame()
+
+for(i in 2:length(dates_o)){ 
+  sub <- okaro_al %>% 
+    filter(date <= dates_o[i] & date > dates_o[i-1]) %>% 
+    mutate(sum_alum = sum(alum), 
+           sample_date = dates_o[i]) %>% 
+    select(-alum)
+  al_sum_okaro <- rbind(al_sum_okaro, sub)
+  
+}
+
+al_sum_okaro <- al_sum_okaro %>% 
+  distinct(sample_date, .keep_all = TRUE) %>% 
+  select(-date) %>% 
+  rename(date = sample_date)
+
+ggplot(al_sum_okaro, aes(x = as.Date(date), y  = sum_alum)) +
+  geom_point(size = 2) +
+  geom_line()
+  
 
 ############################################################################################
 # combine into one data file
+al_both <- full_join(al_sum, al_sum_okaro, by = c('date', 'lake', 'sum_alum', 'note'))
+
+ggplot(al_both, aes(x = as.Date(date), y = sum_alum)) +
+  geom_point() +
+  facet_wrap(~lake, scales = 'free')
+
+############################################################################################
+write.csv(al_both, './data/processed_data/alum_rotoehu_okaro.csv', row.names = FALSE)
+
