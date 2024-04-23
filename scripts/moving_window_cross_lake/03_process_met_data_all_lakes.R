2c3661e7614c98153ff81803d1aa69c4bab314f9
Whitney Woelmer <wwoelmer@vt.edu>
2024-04-23 13:15:01 +1200

    process met data across lakes

--- scripts/moving_window_cross_lake/03_process_met_data_all_lakes.R
+++ scripts/moving_window_cross_lake/03_process_met_data_all_lakes.R
@@ -1,5 +1,45 @@
 # process met data across all lakes
 # data coverage from 2000-2020 for all lakes
+# data are from ERA5
 
-# is ERA5 data from Rotoehu good enough for all the lakes 
-# or is it spatially refined enough to need different data for rotorua, okaro, and tarawera?
+library(tidyverse)
+
+fls <- list.files('./data/moving_window_analysis_cross_lake/met_data/originals')
+fls
+
+for(i in 1:length(fls)){
+  met <- read.csv(file.path('./data/moving_window_analysis_cross_lake/met_data', fls[i]))
+  met <- met %>% 
+    mutate(windspeed = sqrt(MET_wnduvu^2 + MET_wnduvv^2)) %>% 
+    select(Date, MET_tmpair, windspeed, MET_pprain) %>% 
+    rename(date = Date)
+  met$date <- as.Date(met$date)
+  
+  colnames(met) <- c('date', 'air_temp', 'windspeed', 'rain')
+  
+  x <- str_split(fls[i], '[-_]')[[1]][1]
+  met$lake <- x
+  
+  # calculate max, min, mean metrics
+  met_summ <- met %>% 
+    mutate(month = month(date),
+           year = year(date)) %>% 
+    group_by(month, year) %>% 
+    summarise_at(vars(c('air_temp', 'windspeed')), 
+                 list(min = min, mean = mean,  max = max))  
+  
+  rain_summ <- met %>% 
+    mutate(month = month(date),
+           year = year(date)) %>% 
+    group_by(month, year) %>% 
+    summarise(rain_sum = sum(rain))  
+  
+  all_sum <- left_join(met_summ, rain_summ)
+  
+  write.csv(met, paste0('./data/moving_window_analysis_cross_lake/met_data/', x,'_met_', min(year(met$date)), "_", 
+                        max(year(met$date)), '.csv'), row.names = FALSE)
+  write.csv(all_sum, paste0('./data/moving_window_analysis_cross_lake/met_data/', x,'_met_summaries',
+                            min(year(met$date)), "_", 
+                            max(year(met$date)), '.csv'), row.names = FALSE)
+  
+}
