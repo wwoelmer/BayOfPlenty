1221847af19b83d74dd92c2a96fb7be455bc9398
Whitney Woelmer <wwoelmer@vt.edu>
2024-09-05 14:36:24 +1200

    combine all ctd data together

--- scripts/1991_2021_analysis/02_process_ysi_calc_mixing_metrics.R
+++ scripts/1991_2021_analysis/02_process_ysi_calc_mixing_metrics.R
@@ -10,24 +10,57 @@ ysi <- ysi %>%
          depth_m = 'Depth (m)',
          temp_C = 'Temperature (degC)',
          DO_psat = 'DO%',
-         DO_mgl = 'DO (mg/l)')
+         DO_mgL = 'DO (mg/l)')
+ysi$date <- as.Date(ysi$date)
 
+ctd <- read.csv('./data/processed_data/ctd_2021_2024_rotoehu.csv') %>% 
+  mutate(date = as.Date(date)) %>% 
+  select(-LocationName) %>% 
+  rename(DO_mgL = DO_mgl)
+ctd2 <- read.csv('./data/processed_data/BoP_ctd_2003_2022.csv') %>% 
+  filter(lake=='Rotoehu') %>% 
+  rename(chl_ugL = chla_ugL,
+         DO_mgL = DO_gm3,
+         DO_psat = DO_sat,
+         PAR = PAR_umolm2s,
+         Conductivity = spcond_uScm,
+         turb_NTU = turbidity_ntu) %>% 
+  select(-lake, -site, -method, -time) %>% 
+  mutate(date = as.Date(date),
+         depth_m = as.integer(depth_m))
 
-ggplot(ysi, aes(x = as.Date(date), y = temp_C, color = as.factor(depth_m))) +
+ctd3 <- full_join(ctd, ctd2)
+ctd4 <- ctd3 %>% 
+  distinct(date, depth_m, .keep_all = TRUE)
+
+ysi2 <- full_join(ctd4, ysi)
+ysi2 <- ysi2 %>% 
+  arrange(date)
+
+ggplot(ysi2, aes(x = as.Date(date), y = temp_C, color = as.factor(depth_m))) +
   geom_line() +
   geom_point()
 
-ggplot(ysi, aes(x = as.Date(date), y = DO_mgl, color = as.factor(depth_m))) +
+ggplot(ysi2, aes(x = as.Date(date), y = DO_mgL, color = as.factor(depth_m))) +
   geom_line() +
   geom_point()
+
+# get rid of a couple weird dates
+ysi2 <- ysi2 %>% 
+  filter(!date %in% c(as.Date('2020-06-17'), as.Date('2021-04-21')))
+
+
+write.csv(ysi2, './data/processed_data/rotoehu_ctd_1990_2024.csv', row.names = FALSE)
 
 # extract surface and bottom temps for analysis
-temps <- ysi %>% 
+temps <- ysi2 %>% 
   filter(depth_m==0|depth_m==8) %>% 
   select(date, depth_m, temp_C) %>% 
   distinct(date, depth_m, .keep_all = TRUE) %>% 
   pivot_wider(names_from = depth_m, values_from = temp_C, 
               names_prefix = 'temp_')
+
+
 
 #################################################################
 # calculate thermocline depth and schmidt stability
@@ -53,15 +86,15 @@ bty_interp <- data.frame(lake = bty$lake[1],
 # calculate volumetrically weighted DO
 
 
-hypo_o2 <- ysi %>% 
-  select(date, depth_m, temp_C, DO_mgl) %>% 
+hypo_o2 <- ysi2 %>% 
+  select(date, depth_m, temp_C, DO_mgL) %>% 
   distinct(date, depth_m, .keep_all = TRUE) %>% 
   group_by(date) %>% 
   mutate(hypo_depth = meta.depths(temp_C, depth_m, seasonal = FALSE, mixed.cutoff = 0)[2]) %>% 
   group_by(date) %>% 
   filter(depth_m > hypo_depth) %>% 
   left_join(bty_interp, by = 'depth_m') %>% 
-  mutate(DO_vol = DO_mgl*vol_at_contour_m3)
+  mutate(DO_vol = DO_mgL*vol_at_contour_m3)
 
 hypo_VW <- hypo_o2 %>% 
   group_by(date) %>% 
@@ -75,8 +108,8 @@ ggplot(hypo_VW, aes(x = as.Date(date), y = DO_sum)) +
 
 ##################################################################################
 
-t_metrics <- ysi %>% 
-  select(date, depth_m, temp_C, DO_mgl) %>% 
+t_metrics <- ysi2 %>% 
+  select(date, depth_m, temp_C, DO_mgL) %>% 
   distinct(date, depth_m, .keep_all = TRUE) %>% 
   group_by(date) %>% 
   mutate(thermo_depth = thermo.depth(temp_C, depth_m, seasonal = FALSE),
