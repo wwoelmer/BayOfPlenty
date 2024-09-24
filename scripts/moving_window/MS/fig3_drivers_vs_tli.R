99277d01029d8aecb238c07a42244883e7b3847e
wwoelmer <wwoelmer@vt.edu>
2024-09-25 08:57:53 +1200

    rename

--- scripts/moving_window/MS/fig3_drivers_vs_tli.R
+++ scripts/moving_window/MS/fig3_drivers_vs_tli.R
@@ -30,9 +30,9 @@ data_long$variable <- factor(data_long$variable,
                        levels = c("bottom_DRP_ugL", "bottom_NH4_ugL", "temp_C_8",
                                   "air_temp_mean", "windspeed_min", "monthly_avg_level_m",
                                   "schmidt_stability", "sum_alum"),
-                       labels = c("bottom DRP", "bottom NH4", "bottom water temp",
-                                  "mean air temp", "min windspeed", "monthly water level", 
-                                  "schmidt stability", "alum dosed"))
+                       labels = c("Bottom DRP (µg/L)", "Bottom NH4 (µg/L)", "Bottom Water Temp (°C)",
+                                  "Mean Air Temp (°C)", "Min Windspeed (m2/s)", "Water Level (m)", 
+                                  "Schmidt Stability (J/m2)", "Alum Dosed (L/day)"))
 
 # check for normality
 ggplot(data_long, aes(x = value, color = variable)) +
@@ -42,8 +42,8 @@ ggplot(data_long, aes(x = value, color = variable)) +
   theme_bw()
 
 data_long %>% 
-  mutate(value = ifelse(variable %in% c('bottom DRP', 'bottom NH4', 
-                                        'schmidt stability'), log(value), value)) %>% 
+  mutate(value = ifelse(variable %in% c("Bottom DRP (µg/L)", "Bottom NH4 (µg/L)",
+                                        "Schmidt Stability (J/m2)"), log(value), value)) %>% 
 ggplot(aes(x = value, color = variable)) +
   geom_histogram() +
   facet_wrap(~variable, scales = 'free') +
@@ -84,14 +84,17 @@ whole <- ggplot(data_long, aes(x = value, y = tli_monthly, color = variable)) +
             hjust = 1.1, vjust = 2, size = 4, color = "gray3") +
   theme_bw() +
   labs(x = 'Value', y = 'Monthly TLI',
-       color = 'Variable')
+       color = 'Variable') +
+  theme(legend.position = 'none')
 whole
 
+ggsave('./figures/moving_window/MS/drivers_vs_tli_whole_timeseries.png', whole,
+       dpi = 300, units = 'mm', height = 450, width = 450, scale = 0.4)
 ####################################################################################
 # look at this during a time period where a non-significant variable increased in importance (e.g., water level)
 dat_sub <- data_long %>% 
   filter(date > as.Date('2007-07-20') & date < as.Date('2015-11-20')) %>% 
-  select(-significant, -p.value)
+  select(-significant, -p.value, -pearson, -cor_signif)
 
 # Fit linear models for each group and get p-values
 model_sub <- dat_sub %>%
@@ -129,3 +132,47 @@ part <- ggplot(dat_sub2, aes(x = value, y = tli_monthly, color = variable)) +
        color = 'Variable')
 part
 ggarrange(whole, part, common.legend = TRUE)
+
+
+##################################################################################
+# look at a later time frame
+dat_sub <- data_long %>% 
+  filter(date > as.Date('2018-01-01')) %>% 
+  select(-significant, -p.value, -pearson, -cor_signif)
+
+# Fit linear models for each group and get p-values
+model_sub <- dat_sub %>%
+  group_by(variable) %>%
+  do(tidy(lm(tli_monthly ~ value, data = .))) 
+
+cor_sub <- dat_sub %>% 
+  drop_na() %>% 
+  group_by(variable) %>% 
+  summarise(pearson = cor(tli_monthly, value),
+            cor_signif = cor.test(tli_monthly, value)$p.value)
+cor_sub  
+
+# Add significance column to the original dataset
+significance_info <- model_sub %>%
+  filter(term == "value") %>%
+  select(variable, p.value) %>%
+  mutate(significant = p.value < 0.05)
+
+dat_sub2 <- dat_sub %>%
+  left_join(significance_info, by = "variable") %>% 
+  left_join(cor_sub, by = 'variable') %>% 
+  mutate(significant = ifelse(is.na(significant), FALSE, significant)) 
+
+# Plot using ggplot2 with facet_wrap
+part <- ggplot(dat_sub2, aes(x = value, y = tli_monthly, color = variable)) +
+  geom_point() +
+  facet_wrap(~variable, scales = 'free') +
+  geom_smooth(data = filter(dat_sub2, cor_signif < 0.05), method = "lm") +
+  scale_color_manual(values = col_pal) +
+  geom_text(aes(x = Inf, y = Inf, label = round(pearson, 2)), 
+            hjust = 1.1, vjust = 2, size = 4, color = "gray3") +
+  theme_bw() +
+  labs(x = 'Value', y = 'Monthly TLI',
+       color = 'Variable') +
+  ggtitle('Relationships with data from 2018-2023')
+part
